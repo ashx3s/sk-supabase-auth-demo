18bc0f7f4a7546d8203551873b083596f82a0fe0
Ashlyn Knox <ashlyn@lilyx.io>
2023-12-05 12:12:57 -0700

    setup dependencies
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script>
 	import '../app.postcss';
-
-	import AppShell from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
 <AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead"><Icon icon="fa6-brands:empire" /></svelte:fragment>
+			Empire Forms
+			<svelte:fragment slot="trail"><button>Login</button><button>Sign up</button></svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
 	<slot />
+	<svelte:fragment slot="pageFooter"><p>&copy; 2023 Lilyx</p></svelte:fragment>
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+<main>
+	<header class="text-center lg:px-8 lg:py-20">
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+				>Sign up Now</span
+			>
+		</h1>
+	</header>
+</main>
