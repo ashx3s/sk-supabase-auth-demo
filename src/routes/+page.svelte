854dd4935e59d793a44c041c216268bd0cbffc6a
Ashlyn Knox <ashlyn@lilyx.io>
2023-12-06 14:40:05 -0700

    render todos from directus
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,30 @@
+<script>
+	export let data;
+	let { todos } = data;
+</script>
+
 <main>
 	<header class="text-center lg:px-8 lg:py-20">
 		<h1 class="h1">
 			<span
 				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
-				>Sign up Now</span
+				>Home Page</span
 			>
 		</h1>
 	</header>
+	<section>
+		<header>
+			<h2 class="h2">Show All Todos</h2>
+		</header>
+		<div>
+			<ul class="list-type-disc">
+				{#each todos as { title, description }}
+					<li class="ml-8 my-4">
+						<h3 class="h3 font-bold">{title}</h3>
+						<p class="p">{description}</p>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</section>
 </main>
---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let data;
+
+	let { posts } = data;
+</script>
+
+<main>
+	<header class="text-center lg:px-8 lg:py-20">
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+				>Log In</span
+			>
+		</h1>
+	</header>
+	<section>
+		<form action="POST" />
+	</section>
+</main>
